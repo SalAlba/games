8ddc33bf932c2ab33c41fbcc80051afb232ddac4
Pawel Dulak <pawel.dulak@gmail.com>
2020-06-03 10:14:42 +0200

    implemented player learning engine

--- python/connect4/main.py
+++ python/connect4/main.py
@@ -2,6 +2,7 @@ import numpy as np
 import pygame
 import sys
 import math
+import pickle
 
 from sal_timer import timer
 
@@ -62,7 +63,7 @@ class Board:
 
         # factor ....
         self.board = self.create_board()
-        self.print_board()
+        #self.print_board()
 
     def create_board(self):
         return np.zeros((self.ROW_COUNT, self.COLUMN_COUNT))
@@ -75,8 +76,17 @@ class Board:
         print('CURRENT PLAYER : <{}>'.format(Constant.PLAYER_TURN))
         print(self.get_board_hash())
 
+    def possible_moves(self):
+        list_of_moves = []
+        for c in range(0, self.COLUMN_COUNT):
+            if self.board[self.ROW_COUNT-1][c] == Constant.NO_PLAYER_PIECE_VALUE:
+                list_of_moves.append(c)
+        
+        return list_of_moves
+
     def get_board_hash(self):
-        return self.board.flatten()
+        # return self.board.flatten()
+        return str(self.board.reshape(Constant.ROW_COUNT * Constant.COLUMN_COUNT))
 
     def draw_board(self):
         # ...
@@ -243,14 +253,20 @@ class Judge:
 
 
 class Player:
-    def __init__(self, name, isBot=False, isIntelligent=False, model=''):
+    def __init__(self, name, isBot=False, isIntelligent=False, model='', exp_rate=0.3):
         self.name = name
         self.isBot = isBot
         self.isIntelligent = isIntelligent
 
-    def ask_for_move(self):
+        self.states = []  # record all positions taken
+        self.lr = 0.2
+        self.exp_rate = exp_rate
+        self.decay_gamma = 1
+        self.states_value = {}  # state -> value
+
+    def ask_for_move(self, board):
         if self.isBot:
-            return self.ask_bot()
+            return self.ask_bot(board)
         else:
             return self.ask_human()
 
@@ -267,41 +283,100 @@ class Player:
 
                 if event.unicode == '0':
                     return int(event.unicode)
-                    # self.handel_action(event.unicode)
+                    # self.handle_action(event.unicode)
 
                 if event.unicode == '1':
                     return int(event.unicode)
-                    # self.handel_action(event.unicode)
+                    # self.handle_action(event.unicode)
 
                 if event.unicode == '2':
                     return int(event.unicode)
-                    # self.handel_action(event.unicode)
+                    # self.handle_action(event.unicode)
 
                 if event.unicode == '3':
                     return int(event.unicode)
-                    # self.handel_action(event.unicode)
+                    # self.handle_action(event.unicode)
 
                 if event.unicode == '4':
                     return int(event.unicode)
-                    # self.handel_action(event.unicode)
+                    # self.handle_action(event.unicode)
 
                 if event.unicode == '5':
                     return int(event.unicode)
-                    # self.handel_action(event.unicode)
+                    # self.handle_action(event.unicode)
 
                 if event.unicode == '6':
                     return int(event.unicode)
-                    # self.handel_action(event.unicode)
+                    # self.handle_action(event.unicode)
         return Constant.PIECE_VALUE_TO_IGNORE
 
-    def ask_bot(self):
+    def ask_bot(self, board):
         if self.isIntelligent:
             pass
         else:
-            return self.random_choice()
+            board_hash = self.get_board_hash(board)
+            action = self.random_choice()
+            possible_moves = board.possible_moves()
+
+            if len(possible_moves) > 0:
+
+                if np.random.uniform(0, 1) <= self.exp_rate:
+                    # random action already selected
+                    idx = np.random.choice(len(possible_moves))
+                    action = possible_moves[idx]
+
+                else:
+                    value_max = -999
+                    
+                    for p in possible_moves:
+                        next_boardHash = board_hash + ',' + str(p)
+                        value = 0 if self.states_value.get(
+                            next_boardHash) is None else self.states_value.get(next_boardHash)
+                        if value >= value_max:
+                            value_max = value
+                            action = p
+                        if value_max == 0:
+                            idx = np.random.choice(len(possible_moves))
+                            action = possible_moves[idx]
+
+            # save the move
+            next_boardHash = board_hash + ',' + str(action)
+            self.addState(next_boardHash)
+
+            #print("{} takes action {}".format(self.name, action))
+            return action
 
     def random_choice(self):
         return np.random.choice(list(range(0, Constant.COLUMN_COUNT)))
+
+    def get_board_hash(self, board):
+        return board.get_board_hash()
+
+    def addState(self, state):
+        self.states.append(state)
+
+    # at the end of game, backpropagate and update states value
+    def feedReward(self, reward):
+        # print("{} received reward {}".format(self.name, reward))
+        for st in reversed(self.states):
+            if self.states_value.get(st) is None:
+                self.states_value[st] = 0
+            self.states_value[st] += self.lr * \
+                (self.decay_gamma * reward - self.states_value[st])
+            reward = self.states_value[st]
+
+    def reset(self):
+        self.states = []
+
+    def savePolicy(self):
+        fw = open('policy_' + str(self.name), 'wb')
+        pickle.dump(self.states_value, fw)
+        fw.close()
+
+    def loadPolicy(self, file):
+        fr = open(file, 'rb')
+        self.states_value = pickle.load(fr)
+        fr.close()
 
 
 class Game:
@@ -310,6 +385,7 @@ class Game:
         self.p1 = player1
         self.p2 = player2
         self.CHANGE_PLAYER = False
+        Constant.GAME_OVER = False
 
         # ...
         pygame.init()
@@ -323,13 +399,17 @@ class Game:
 
     def check_winners(self):
         if self.judge.winning_move(Constant.get_current_piece_value()):
-            # label = myfont.render("Player 1 wins!!", 1, const.RED)
-            # screen.blit(label, (40, 10))
-            print('<>'*20)
-            print('PLayer {} win !!'.format(Constant.PLAYER_TURN))
-            print('<>'*20)
             Constant.GAME_OVER = True
             self.board.print_winner_player_name()
+            # feed rewards
+            self.get_current_player().feedReward(1)
+            self.get_not_current_player().feedReward(0)
+        if len(self.board.possible_moves()) == 0:
+            Constant.GAME_OVER = True
+            # no more moves, draw!
+            print("No more moves!!!")
+            self.get_current_player().feedReward(0.1)
+            self.get_not_current_player().feedReward(0.2)
 
     def check_game_over(self):
         if Constant.GAME_OVER:
@@ -340,7 +420,7 @@ class Game:
             Constant.PLAYER_TURN += 1
             Constant.PLAYER_TURN = Constant.PLAYER_TURN % 2
             self.CHANGE_PLAYER = False
-            self.board.print_board()
+            #self.board.print_board()
 
     def get_current_player(self):
         return {
@@ -348,7 +428,13 @@ class Game:
             '1': self.p2,
         }.get(str(Constant.PLAYER_TURN), None)
 
-    def handel_action(self, selected_col_by_player):
+    def get_not_current_player(self):
+        return {
+            '0': self.p2,
+            '1': self.p1,
+        }.get(str(Constant.PLAYER_TURN), None)
+
+    def handle_action(self, selected_col_by_player):
         # ...
         if selected_col_by_player == Constant.PIECE_VALUE_TO_IGNORE:
             return
@@ -371,42 +457,56 @@ class Game:
             self.CHANGE_PLAYER = True
         else:
             # NOTE maybe her we should add points
-            print(">>> Pleas select another column current is full <<<")
+            print(">>> Please select another column current is full <<<")
             print(">>> Still player <{}> turn <<<".format(Constant.PLAYER_TURN))
             self.CHANGE_PLAYER = False
 
-    def start_game(self):
+    def start_game(self, isTraining=False):
         while not Constant.GAME_OVER:
             selected_col_by_player = Constant.PIECE_VALUE_TO_IGNORE
-            selected_col_by_player = self.get_current_player().ask_for_move()
-            self.handel_action(selected_col_by_player)
+            selected_col_by_player = self.get_current_player().ask_for_move(self.board)
+            self.handle_action(selected_col_by_player)
 
             # TODO ...
             self.board.draw_header()
-            self.board.refresh()
+            if not isTraining:
+                self.board.refresh()
             self.check_player_turn()
-            self.check_game_over()
+            if not isTraining:
+                self.check_game_over()
 
 @timer
 def main():
     print('Welcome To Game Mind')
+
     # Bot VS Bot ...
-    # bot1 = Player('Bot1', isBot=True, isIntelligent=False)
-    # bot2 = Player('Bot2', isBot=True, isIntelligent=False)
-    # bots_game = Game(bot1, bot2)
-    # bots_game.start_game()
+    bot1 = Player('Bot1', isBot=True, isIntelligent=False)
+    bot2 = Player('Bot2', isBot=True, isIntelligent=False)
+
+    for r in range(50000):
+        print('Round {}'.format(r))
+        bots_game = Game(bot1, bot2)
+        bots_game.start_game(isTraining=True)
+        # reset game
+        bot1.reset()
+        bot2.reset()
+        Constant.PLAYER_TURN = 0
+
+    bot1.savePolicy()
+    bot2.savePolicy()
 
     # Human VS Bot...
-    # player1 = Player('P1')
-    # player_bot = Player('Bot', isBot=True, isIntelligent=False)
-    # humans_game = Game(player1, player_bot)
-    # humans_game.start_game()
-
-    # Human VS Human...
-    player1 = Player('P1')
-    player2 = Player('p1')
+    player1 = Player('Bot1', isBot=True, isIntelligent=False, exp_rate=0)
+    player1.loadPolicy('policy_Bot1')
+    player2 = Player('Human')
     humans_game = Game(player1, player2)
     humans_game.start_game()
+
+    # Human VS Human...
+    # player1 = Player('P1')
+    # player2 = Player('p1')
+    # humans_game = Game(player1, player2)
+    # humans_game.start_game()
 
 
 if __name__ == '__main__':
